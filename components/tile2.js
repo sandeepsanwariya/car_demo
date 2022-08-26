7caca3775b2159776bed60ffb8701e498c1b6f66
sandeepsanwariya <sk.512go@gmail.com>
2022-08-26 20:09:28 +0530

    'asas'

--- components/tile2.js
+++ components/tile2.js
@@ -9,13 +9,24 @@ import Pic1 from './res2/1.png'
 import Pic2 from './res2/2.png'
 import Pic3 from './res2/3.png'
 import Pic4 from './res2/4.png'
+import { ClipLoader } from 'react-spinners';
 export default function Tile() {
   const [images, setimages] = useState([])
+  const [show, setshow] = useState(false)
+  useEffect(() => {
+    setshow(false)
+    const timer = setTimeout(() => {
+      setshow(true)
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [images]);
 
   return (<>
   
-     
-      <div className='bg-gray-700  h-auto p-4 pb-1 rounded-lg hover:bg-gray-900 hover:scale-105 ease-in-out duration-300 active:bg-gray-700 focus:outline-none focus:ring focus:ring-black m-5 text-white' >
+     <div className='flex justify-center'>
+
+
+      <div className='bg-gray-700 max-w-[700px]  h-auto p-4 pb-1 rounded-lg hover:bg-gray-900 hover:scale-105 ease-in-out duration-300 active:bg-gray-700 focus:outline-none focus:ring focus:ring-black m-5 text-white' >
         <p className='text-xl italic' >UPLOAD IMAGE HERE</p>
         <div className='flex flex-row pt-2 align-bottom  justify-between w-full h-full'>
        
@@ -28,13 +39,14 @@ export default function Tile() {
       <br/>
       <br/>
       <br/>
-      {images.length>0 && (<>
+      { images.length>0 && (<>
     
         <div className='bg-gray-700  h-auto p-4 pb-1 rounded-lg hover:bg-gray-900 hover:scale-105 ease-in-out duration-300 active:bg-gray-700 focus:outline-none focus:ring focus:ring-black m-5 text-white' >
     
    
        <h2>Result</h2>
-       {images[0].name=='1.png' && (<>
+       {show ?(<>
+        {images[0].name=='1.png' && (<>
          <Image width={'400px'} height={'400px'}  className='max-h-[400px] max-w-[400px] '    src={Pic1}/>
        </>)}
        {images[0].name=='2.png' && (<>
@@ -46,8 +58,14 @@ export default function Tile() {
        {images[0].name=='4.png' && (<>
          <Image width={'400px'} height={'400px'}  className='max-h-[400px] max-w-[400px] '    src={Pic4}/>
        </>)}
+       </>):(<>
+        <ClipLoader color={"#1a2238"} loading={!show} size={250} /> 
+          </>)}
+     
      </div>
+
       </>)}
+      </div>
      
              
       </>  )
